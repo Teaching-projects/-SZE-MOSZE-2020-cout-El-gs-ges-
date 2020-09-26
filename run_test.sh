36d6c9f80e4413912191fb0c8de629988be50857
Doncsenko Roman <43998554+DoncsenkoRoman@users.noreply.github.com>
2020-09-26 17:49:31 +0200

    new-sh

--- run_test.sh
+++ run_test.sh
@@ -1,7 +1,11 @@
-#!/bin/bash
+#!/bin/sh
 
-IFS=$'\n'
 
-for i in `cat input.txt`; do 
-    echo $i | xargs ./a.out >> generated_output.txt
-done
+touch generated_output.txt
+
+./a.out Units/character1.json Units/character2.json >> generated_output.txt
+./a.out Units/character1.json Units/character3.json >> generated_output.txt
+./a.out Units/character2.json Units/character1.json >> generated_output.txt
+./a.out Units/character2.json Units/character3.json >> generated_output.txt
+./a.out Units/character3.json Units/character1.json >> generated_output.txt
+./a.out Units/character3.json Units/character2.json >> generated_output.txt
