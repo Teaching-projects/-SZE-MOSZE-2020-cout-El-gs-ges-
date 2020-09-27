a47a565cb08ddb4f657ba44563ac92a6e4ef1ba3
Doncsenko Roman <43998554+DoncsenkoRoman@users.noreply.github.com>
2020-09-27 08:08:54 +0200

    new-path

--- run_test.sh
+++ run_test.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 IFS=$'\n'
 
-./a.out Units/character1.json Units/character2.json >> generated_output.txt
-./a.out Units/character1.json Units/character3.json >> generated_output.txt
-./a.out Units/character2.json Units/character1.json >> generated_output.txt
-./a.out Units/character2.json Units/character3.json >> generated_output.txt
-./a.out Units/character3.json Units/character1.json >> generated_output.txt
-./a.out Units/character3.json Units/character2.json >> generated_output.txt
+./a.out Units/Units/character1.json Units/Units/character2.json >> generated_output.txt
+./a.out Units/Units/character1.json Units/Units/character3.json >> generated_output.txt
+./a.out Units/Units/character2.json Units/Units/character1.json >> generated_output.txt
+./a.out Units/Units/character2.json Units/Units/character3.json >> generated_output.txt
+./a.out Units/Units/character3.json Units/Units/character1.json >> generated_output.txt
+./a.out Units/Units/character3.json Units/Units/character2.json >> generated_output.txt
 
 cat generated_output.txt
