752af92d0bff11329c05661bc218edde25c60c09
Doncsenko Roman <43998554+DoncsenkoRoman@users.noreply.github.com>
2020-09-29 10:32:50 +0200

    sh-delete-cat

--- run_test.sh
+++ run_test.sh
@@ -7,5 +7,3 @@ IFS=$'\n'
 ./a.out Units/Units/character2.json Units/Units/character3.json >> generated_output.txt
 ./a.out Units/Units/character3.json Units/Units/character1.json >> generated_output.txt
 ./a.out Units/Units/character3.json Units/Units/character2.json >> generated_output.txt
-
-cat generated_output.txt
